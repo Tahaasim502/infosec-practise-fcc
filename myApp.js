dbf2f3c2d26dafa6e5d8ffc9d62184cf5d70e4a0
Taha Asim <tahaasim40@gmail.com>
2025-08-10 12:49:11 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,30 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+app.use(helmet.hidePoweredBy()); //this will hide the X-Powered-By header -> so that attackers cannot see what technology is being used
 
+
+
+//here we are check if process.env.port can read a port number from the environment 
+//if it cannot read the port number we are going to use 3030, so its a default set
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Infosec practise App started on port ${port}`);
+});
 
 
 //here we are check if process.env.port can read a port number from the environment 
