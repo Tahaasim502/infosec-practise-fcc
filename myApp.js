6a88df5f76aaa9128ef0b2dba73e88861d9bc9fd
Tahaasim502 <tahaasim40@gmail.com>
2025-08-10 15:38:33 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,4 @@
-const express = require('express');
-const helmet = require('helmet');
-const app = express();
+
 
 const express = require('express');
 const helmet = require('helmet');
@@ -28,20 +26,3 @@ app.listen(port, () => {
 });
 
 
-//here we are check if process.env.port can read a port number from the environment 
-//if it cannot read the port number we are going to use 3030, so its a default set
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Infosec practise App started on port ${port}`);
-});
